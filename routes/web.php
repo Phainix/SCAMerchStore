a33c6a22fc140e897b7e17123ce0e9704297e5bc
Adekogbe Faith <28295747+Phainix@users.noreply.github.com>
2020-01-15 22:56:21 +0100

    Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,41 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic Route
+Route::get('/home', function() {
+    return 'This is the home page!!!';
+});
+
+// Required Param Route
+Route::get('/basic/{name}', function($name) {
+    return "Welcome home $name, its glad to have you here";
+});
+
+// Optional Param Routes
+Route::get('/optional/param/{age?}', function($age = null) {
+    return "Hi there, this is an optional param route, this means you can omit route paramaters. On this page you entered " . ($age == null ? "nothing" : $age);
+});
+
+Route::get('/optional/default/param/{age?}', function($age = 50) {
+    return "Hi there, this is an optional param route with a default value, this means you can omit route paramaters. On this page you entered $age";
+});
+
+// Routes with param constraints
+
+Route::get('/param/constraint/{age}', function($age) {
+    return "This route expects your age and should only permit integers as the route param. You entered Age: $age. It " . (preg_match("/^\d+$/",$age) ? "worked" : "did not work");
+})->where('age', '[0-9]+');
+
+Route::get('/param/constraint/{name}', function($name) {
+    return "This route expects your name and should only permit alphabets as the route param. You entered Name: $name. It " . (preg_match("/^([a-zA-Z']+)$/",$name) ? "worked" : "did not work");
+})->where('name', '[a-z]+');
+
+// Named routes
+Route::get('/named/route', function () {
+    return "This is a named route";
+})->name('named-route');
+
+Route::get('/named/route/test', function () {
+    return "<a href='" . route('named-route') ."'>This is a redirects to a route</a>";
+});
